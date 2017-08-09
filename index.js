cfbb469d0cae6a85ac2bd9987fbdb393e22fc904
Sergey Ilyevsky <sergeyi@wix.com>
2017-08-09 15:39:55 +0300

    .

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import RN from 'react-native';
 import LibQ from 'purple-lib-q';
+import LibR from 'purple-lib-r';
 
 var _engine;
 
@@ -11,7 +12,6 @@ class ModuleComponent extends Component {
         <RN.Text style={{
             fontSize: 16,
             textAlign: 'center',
-            margin: 10,
             color: 'purple'
           }}>
           PurpleModB
@@ -19,10 +19,11 @@ class ModuleComponent extends Component {
         <RN.Text style={{
             fontSize: 14,
             textAlign: 'center',
-            margin: 10,
             color: 'blue'
           }}>
-          lib-q version: {LibQ.version()}
+          lib-q version: {LibQ.version()}{'\n'}
+          lib-r version: {LibR.version()}{'\n'}
+          lib-q used by lib-r: {LibR.libQVersion()}
         </RN.Text>
       </RN.View>
     );
